8395498a0ed0548cb058865137d642020cc5c4f7
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-06-17 23:24:42 -0300

    O Projeto
    All Project in one commit

---
+++ BDSimples/Cadastro.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace BDSimples
+{
+    partial class Cadastros
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cadastros));
+            nome = new Label();
+            nameBox = new TextBox();
+            sexo = new Label();
+            cidade = new Label();
+            cityBox = new TextBox();
+            btn_incluir = new Button();
+            btn_alterar = new Button();
+            btn_excluir = new Button();
+            btn_contar = new Button();
+            F = new RadioButton();
+            panel1 = new Panel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            panel4 = new Panel();
+            analiseText = new TextBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel2 = new Panel();
+            dgvSearch = new DataGridView();
+            panel3 = new Panel();
+            M = new RadioButton();
+            panel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            panel4.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvSearch).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // nome
+            // 
+            resources.ApplyResources(nome, "nome");
+            nome.Name = "nome";
+            // 
+            // nameBox
+            // 
+            resources.ApplyResources(nameBox, "nameBox");
+            nameBox.BorderStyle = BorderStyle.FixedSingle;
+            nameBox.Cursor = Cursors.IBeam;
+            nameBox.Name = "nameBox";
+            // 
+            // sexo
+            // 
+            resources.ApplyResources(sexo, "sexo");
+            sexo.Name = "sexo";
+            // 
+            // cidade
+            // 
+            resources.ApplyResources(cidade, "cidade");
+            cidade.Name = "cidade";
+            // 
+            // cityBox
+            // 
+            resources.ApplyResources(cityBox, "cityBox");
+            cityBox.AccessibleRole = AccessibleRole.ScrollBar;
+            cityBox.BorderStyle = BorderStyle.FixedSingle;
+            cityBox.Cursor = Cursors.IBeam;
+            cityBox.Name = "cityBox";
+            // 
+            // btn_incluir
+            // 
+            resources.ApplyResources(btn_incluir, "btn_incluir");
+            btn_incluir.Name = "btn_incluir";
+            btn_incluir.UseVisualStyleBackColor = true;
+            btn_incluir.Click += btnIncluir_Click;
+            // 
+            // btn_alterar
+            // 
+            resources.ApplyResources(btn_alterar, "btn_alterar");
+            btn_alterar.Name = "btn_alterar";
+            btn_alterar.UseVisualStyleBackColor = true;
+            btn_alterar.Click += btnAlterar_Click;
+            // 
+            // btn_excluir
+            // 
+            resources.ApplyResources(btn_excluir, "btn_excluir");
+            btn_excluir.Name = "btn_excluir";
+            btn_excluir.UseVisualStyleBackColor = true;
+            btn_excluir.Click += btnExcluir_Click;
+            // 
+            // btn_cont
+            // 
+            resources.ApplyResources(btn_contar, "btn_cont");
+            btn_contar.Name = "btn_cont";
+            btn_contar.UseVisualStyleBackColor = true;
+            btn_contar.Click += btnContar_Click;
+            // 
+            // F
+            // 
+            resources.ApplyResources(F, "F");
+            F.Name = "F";
+            F.TabStop = true;
+            F.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(panel1, "panel1");
+            panel1.Controls.Add(tableLayoutPanel2);
+            panel1.Controls.Add(tableLayoutPanel1);
+            panel1.Name = "panel1";
+            // 
+            // tableLayoutPanel2
+            // 
+            resources.ApplyResources(tableLayoutPanel2, "tableLayoutPanel2");
+            tableLayoutPanel2.Controls.Add(panel4, 0, 0);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            // 
+            // panel4
+            // 
+            resources.ApplyResources(panel4, "panel4");
+            panel4.Controls.Add(analiseText);
+            panel4.Controls.Add(btn_contar);
+            panel4.Name = "panel4";
+            // 
+            // analiseText
+            // 
+            resources.ApplyResources(analiseText, "analiseText");
+            analiseText.Name = "analiseText";
+            analiseText.ReadOnly = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(tableLayoutPanel1, "tableLayoutPanel1");
+            tableLayoutPanel1.Controls.Add(panel2, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel3, 1, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(panel2, "panel2");
+            panel2.Controls.Add(dgvSearch);
+            panel2.Name = "panel2";
+            // 
+            // dgvSearch
+            // 
+            resources.ApplyResources(dgvSearch, "dgvSearch");
+            dgvSearch.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvSearch.Name = "dgvSearch";
+            dgvSearch.ReadOnly = true;
+            dgvSearch.RowTemplate.Height = 25;
+            dgvSearch.DoubleClick += dgvSearch_DoubleClick;
+            // 
+            // panel3
+            // 
+            resources.ApplyResources(panel3, "panel3");
+            panel3.Controls.Add(F);
+            panel3.Controls.Add(btn_excluir);
+            panel3.Controls.Add(nome);
+            panel3.Controls.Add(btn_alterar);
+            panel3.Controls.Add(M);
+            panel3.Controls.Add(btn_incluir);
+            panel3.Controls.Add(sexo);
+            panel3.Controls.Add(cidade);
+            panel3.Controls.Add(nameBox);
+            panel3.Controls.Add(cityBox);
+            panel3.Name = "panel3";
+            // 
+            // M
+            // 
+            resources.ApplyResources(M, "M");
+            M.Name = "M";
+            M.TabStop = true;
+            M.UseVisualStyleBackColor = true;
+            // 
+            // Cadastros
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.None;
+            Controls.Add(panel1);
+            Name = "Cadastros";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvSearch).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label nome;
+        private Label sexo;
+        private Label cidade;
+
+        private TextBox nameBox;
+        private TextBox cityBox;
+        private TextBox analiseText;
+
+        private Button btn_incluir;
+        private Button btn_alterar;
+        private Button btn_excluir;
+        private Button btn_contar;
+
+        private RadioButton F;
+        private RadioButton M;
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel1;
+
+        private DataGridView dgvSearch;
+    }
+}
---
+++ BDSimples/Cadastro.cs
@@ -0,0 +1,281 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace BDSimples
+{
+    public partial class Cadastros : Form
+    {
+        string connstring = "Server=localhost;uid=root;pwd=root;database=auesolucoes";
+        private int userID;
+
+        public Cadastros()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GridFillSearch();
+
+        }
+
+        private void checkIfTextBoxIsEmpty()
+        {
+            if (nameBox.Text == "" || cityBox.Text == "")
+            {
+                throw new Exception("Nome ou cidade se encontra vazio");
+            }
+        }
+
+        private String getCheckedRadioValue()
+        {
+            if (M.Checked == true) return M.Name;
+            if (F.Checked == true) return F.Name;
+            throw new Exception("Sexo não selecionado");
+        }
+
+        void GridFillSearch()
+        {
+            try
+            {
+
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = connstring;
+                con.Open();
+
+                string sql = "SELECT codcontato, nome AS Name, sexo AS Sexo, cidade AS Cidade, date_format(data_cadastro, '%d-%m-%Y') AS 'Data do Cadastro' FROM cadastros";
+
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter(sql, con);
+                DataTable dtblUser = new DataTable();
+
+                sqlDa.Fill(dtblUser);
+                dgvSearch.DataSource = dtblUser;
+                dgvSearch.Columns[0].Visible = false;
+                dgvSearch.Rows[dgvSearch.Rows.Count - 1].Height = -1;
+
+                analiseText.Text = "";
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void headerContar()
+        {
+            try
+            {
+                String allRegistersSQL = "SELECT " +
+                    "count(codcontato) as total, " +
+                    "SUM(CASE WHEN sexo = 'M' THEN 1 ELSE 0 END) AS total_homens, " +
+                    "SUM(CASE WHEN sexo = 'F' THEN 1 ELSE 0 END) AS total_mulheres " +
+                    "FROM cadastros";
+
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = connstring;
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(allRegistersSQL, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    analiseText.Text = "Análise dos contatos: \r\n" +
+                                       " .Número de contatos no banco de dados: "
+                                       + reader.GetString(0) + ", " + reader.GetString(1) +
+                                       " homens e " + reader.GetString(2) + " mulheres";
+                }
+
+                reader.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private List<String> cityList()
+        {
+            String existingCitiesSQL = "SELECT DISTINCT cidade FROM cadastros";
+            MySqlConnection con = new MySqlConnection();
+            con.ConnectionString = connstring;
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand(existingCitiesSQL, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            List<string> citiesList = new List<string>();
+            int index = 0;
+
+            while (reader.Read())
+            {
+                string result = reader.GetString(0);
+                citiesList.Add(result);
+            }
+            reader.Close();
+            return citiesList;
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = connstring;
+                con.Open();
+
+                string sql = "INSERT INTO cadastros (nome, cidade, sexo, data_cadastro) values (@nome , @cidade, @sexo, CURRENT_DATE())";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+
+                checkIfTextBoxIsEmpty();
+
+                cmd.Parameters.AddWithValue("@nome", nameBox.Text);
+                cmd.Parameters.AddWithValue("@cidade", cityBox.Text);
+                cmd.Parameters.AddWithValue("@sexo", getCheckedRadioValue());
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastrado com sucesso");
+                GridFillSearch();
+
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao conectar no banco de dados: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String registerPerMonthSQL = "SET lc_time_names = 'pt_BR';" +
+                    "SELECT " +
+                    "MONTHNAME(data_cadastro) AS mes, " +
+                    "SUM(CASE WHEN sexo = 'M' THEN 1 ELSE 0 END) AS total_homens, " +
+                    "SUM(CASE WHEN sexo = 'F' THEN 1 ELSE 0 END) AS total_mulheres " +
+                    "FROM cadastros where cidade=@city " +
+                    "GROUP BY MONTHNAME(data_cadastro)";
+
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = connstring;
+                con.Open();
+                headerContar();
+
+                List<string> citiesList = cityList();
+                int index = 0;
+
+                while (index < citiesList.Count)
+                {
+                    MySqlCommand cmd = new MySqlCommand(registerPerMonthSQL, con);
+                    cmd.Parameters.AddWithValue("@city", citiesList[index]);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    int cadastroTotalPorCidade = 0;
+
+                    analiseText.Text += "\r\n\r\n.Contatos em " + citiesList[index].ToUpper() + ":";
+                    while (reader.Read())
+                    {
+                        String homem = reader.GetInt32(1) > 1 || reader.GetInt32(1) == 0 ? " homens e " : " homem e ";
+                        String mulher = reader.GetInt32(2) > 1 || reader.GetInt32(2) == 0 ? " mulheres" : " mulher";
+
+                        analiseText.Text += "\r\n" +
+                            (char.ToUpper(reader.GetString(0)[0]) + reader.GetString(0).Substring(1)) +
+                            ": " + (reader.GetInt32(1) + reader.GetInt32(2)) + ", " +
+                            reader.GetInt32(1) + homem + reader.GetInt32(2) + mulher;
+                        cadastroTotalPorCidade += reader.GetInt32(1) + reader.GetInt32(2);
+                    }
+                    analiseText.Text += "\r\nTotal: " + cadastroTotalPorCidade;
+                    reader.Close();
+                    index++;
+
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (dgvSearch.CurrentRow.Index == 0) throw new Exception("Nenhum cadastro selecionado");
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = connstring;
+                con.Open();
+
+                string sql = "DELETE FROM cadastros WHERE codcontato=@id";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+
+                cmd.Parameters.AddWithValue("@id", userID);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Cadastrado excluido");
+
+
+                GridFillSearch();
+                analiseText.Text = "";
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao conectar no banco de dados: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection();
+                con.ConnectionString = connstring;
+                con.Open();
+
+                string sql = "UPDATE cadastros SET nome = @nome, sexo = @sexo, cidade = @cidade WHERE codcontato=@id";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+
+                checkIfTextBoxIsEmpty();
+
+                cmd.Parameters.AddWithValue("@nome", nameBox.Text);
+                cmd.Parameters.AddWithValue("@cidade", cityBox.Text);
+                cmd.Parameters.AddWithValue("@sexo", getCheckedRadioValue());
+                cmd.Parameters.AddWithValue("@id", userID);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Cadastrado atualizado");
+
+                GridFillSearch();
+                analiseText.Text = "";
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao conectar no banco de dados: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dgvSearch_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvSearch.CurrentRow.Index != -1)
+            {
+                nameBox.Text = dgvSearch.CurrentRow.Cells[1].Value.ToString();
+                cityBox.Text = dgvSearch.CurrentRow.Cells[3].Value.ToString();
+                if (dgvSearch.CurrentRow.Cells[2].Value.ToString() == "M") M.Checked = true;
+                else F.Checked = true;
+                userID = Convert.ToInt32(dgvSearch.CurrentRow.Cells[0].Value.ToString());
+            }
+        }
+    }
+}
